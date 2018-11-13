ac82634bc7cd740e7afc0533fcafc967c21c7b39
Alex Huk <alex.huk85@gmail.com>
2018-11-14 06:42:58 +1100

    haha

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output text on screen.
+            Console.WriteLine("My first app in Beginning Visual C# 2018");
+            Console.ReadKey();
+        }
+    }
+}
